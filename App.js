098f27aaa2df8ec748e348807c15224eb917d1e5
Jaegyun Jung <jgiraffe@naver.com>
2020-10-22 04:13:53 +0900

    rename loading.js

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 // import { StatusBar } from "expo-status-bar";
 import React from "react";
+import Alert from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default class App extends React.Component {
   getLocaion = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
   };
   componentDidMount() {
     this.getLocaion();
